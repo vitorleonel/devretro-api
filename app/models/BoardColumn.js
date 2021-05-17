c60b8584121e8c1bc3b6fd573b19317f5d4cc1bb
Vitor Leonel <falecom@vitorleonel.com>
2021-05-16 23:25:19 -0300

    board columns schema

---
+++ app/models/BoardColumn.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const boardColumnItemSchema = new mongoose.Schema(
+  {
+    description: String,
+  },
+  { timestamps: true }
+);
+
+const boardColumnSchema = new mongoose.Schema(
+  {
+    board: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Board',
+    },
+    name: String,
+    items: {
+      type: [boardColumnItemSchema],
+      default: () => [],
+    },
+  },
+  { timestamps: true }
+);
+
+const BoardColumn = mongoose.model(
+  'BoardColumn',
+  boardColumnSchema,
+  'board_columns'
+);
+
+module.exports = BoardColumn;

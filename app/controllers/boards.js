c9f1878011bfa61554d5913820d59f3abfa180b7
Vitor Leonel <falecom@vitorleonel.com>
2021-05-16 23:25:47 -0300

    on create board, add default columns

--- app/controllers/boards.js
+++ app/controllers/boards.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const Board = require('../models/Board');
+const BoardColumn = require('../models/BoardColumn');
 
 module.exports.createBoard = async (req, reply) => {
   try {
     const board = await Board.create({ name: req.body.name });
+
+    await BoardColumn.insertMany([
+      { board: board._id, name: 'Went well' },
+      { board: board._id, name: 'To improve' },
+      { board: board._id, name: 'Action items' },
+    ]);
 
     reply.code(201).send({ board });
   } catch (error) {

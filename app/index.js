6d72f1342e4c048b6640199a767e4681f42e7899
Vitor Leonel <falecom@vitorleonel.com>
2021-05-21 07:06:49 -0300

    organize code

--- app/index.js
+++ app/index.js
@@ -1,19 +1,23 @@
 'use strict';
 
 const fastify = require('fastify');
+const socketIOPlugin = require('fastify-socket.io');
+const corsPlugin = require('fastify-cors');
+
+const mongoosePlugin = require('./plugins/mongoose');
+
+const boardsRoute = require('./routes/boards');
 
 function build(options = {}) {
   const app = fastify(options);
 
   // plugins
-  app.register(require('./plugins/mongoose'), {
-    uri: process.env.DATABASE_URI,
-  });
-  app.register(require('fastify-socket.io'), { cors: { origin: '*' } });
-  app.register(require('fastify-cors'));
+  app.register(mongoosePlugin, { uri: process.env.DATABASE_URI });
+  app.register(socketIOPlugin, { cors: { origin: '*' } });
+  app.register(corsPlugin);
 
   // routes
-  app.register(require('./routes/boards'), { prefix: '/boards' });
+  app.register(boardsRoute, { prefix: '/boards' });
 
   // default route
   app.get('/', async function (_, reply) {
